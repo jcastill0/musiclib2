3a1ef5329b6c3cab7dfbcc34b51d98b4cf7305f2
Julio Castillo <julio.castillo.2010@gmail.com>
2013-06-27 21:24:09 -0700

    started work on playlist

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,14 +3,34 @@
 /* jasmine specs for controllers go here */
 
 describe('controllers', function(){
-  beforeEach(module('myApp.controllers'));
+  beforeEach(module('myMusicLib.controllers'));
 
+  describe('UserDetailCtrl', function() {
+    var scope, ctrl, $httpBackend,
+	userData = function() {
+	  return ({id:1, loginName:'julio', email:'xzy@email.com', firstName:'yo', lastName:'tu'})
+	};
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $routeParams, $controller) {
+	$httpBackend = _$httpBackend_;
+	$httpBackend.expectGET('users/1.json').respond(xyzPhoneData());
 
-  it('should ....', inject(function() {
-    //spec body
-  }));
+	$routeParams.userId = 1;
+	scope = $rootScope.$new();
+	ctrl = $controller(UserDetailCtrl, {$scope: scope});
+    }));
+
+    it('should fetch user detail', inject(function() {
+	expect(scope.user).toEqualData({});
+	$httpBackend.flush();
+	expect(scope.user).toEqualData(userData());	
+    }));
+  });
+
+  describe('', function() {
+    it('should ....', inject(function() {
+      //spec body
+    }));
+  });
+
 });
