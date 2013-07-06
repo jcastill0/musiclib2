8cc20fd528e7c01854ff414d802e3d0f465a8734
Julio Castillo <julio.castillo.2010@gmail.com>
2013-07-06 10:04:29 -0700

    added support for playlist editing

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -22,9 +22,31 @@ app.controller('PlayCtrl', function($scope, $routeParams, Playlist) {
   $scope.playlist = Playlist.get({playlistID:$routeParams.playlistID});
 });
 
-app.controller('PlaylistDetailCtrl', function($scope, $routeParams, Playlist, Song) {
+app.controller('PlaylistDetailCtrl', function($scope, $routeParams, Playlist, Song, $log, $location) {
   $scope.playlist = Playlist.get({playlistID:$routeParams.playlistID});
   $scope.songs = Song.query();
+
+  $scope.addSong = function(song) {
+    $scope.playlist.songs.push(song);	// add to playlist
+  };
+  $scope.removeSong = function(song) {
+    var songIDtoBeRemoved = song.id;
+    var index = 0;
+    angular.forEach($scope.playlist.songs, function(song) {
+	if (song.id == songIDtoBeRemoved) {
+	    $scope.playlist.songs.splice(index, 1);
+	}
+	index++;
+    });
+  };
+
+  $scope.save = function() {
+    $log.log($scope.playlist.name);
+    Playlist.save({playlistID:$routeParams.playlistID}, $scope.playlist, function (playlist) {
+	$log.log(playlist);
+    });
+    $location.path('/');
+  };
 });
 
 app.controller('ArtistCtrl', function($scope, Artist) {
